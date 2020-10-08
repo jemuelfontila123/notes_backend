623e880ac51ab25b6f284518ea86ba44e7968d19
unknown <jemuelfontila123@gmail.com>
2020-10-08 10:15:14 +0800

    added test for adding and posting note

--- models/Note.js
+++ models/Note.js
@@ -6,6 +6,13 @@ const noteSchema = new mongoose.Schema({
     important: Boolean
 })  
 
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 
 
 module.exports = mongoose.model('Note', noteSchema)
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,20 +1,58 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
 const baseUrl = '/api/notes/'
+const Note = require ('../models/Note')
 
-test('notes are returned as json', async () => {
-    const response = await api
-      .get(baseUrl)
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
-test('there is only one note', async () => {
+const initialNotes = [
+    {
+      content: 'HTML is easy',
+      date: new Date(),
+      important: false,
+    },
+    {
+      content: 'Browser can execute only Javascript',
+      date: new Date(),
+      important: true,
+    },
+  ]
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
+
+
+test('all notes are returned', async() => {
     const response = await api.get(baseUrl)
-    expect(response.body).toHaveLength(1)
-        
+
+    expect(response.body).toHaveLength(initialNotes.length)
+})
+test('a specific note is within the returned notes', async () => {
+    const response = await api.get(baseUrl)
+  
+    const contents = response.body.map(r => r.content)
+    expect(contents).toContain(
+      'Browser can execute only Javascript'
+    )
+  })
+test.only('a valid note can be added', async() => {
+    const newNote = {
+        content: 'react is easy',
+        important: true
+    }
+    const addedNote = await api.post(baseUrl)
+                        .send(newNote)
+                        .expect(200)
+                        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get(baseUrl)
+    const contents = response.body.map(note => note.content)
+    expect(response.body).toHaveLength(initialNotes.length+1)
+    expect(contents).toContain('react is easy')
 })
 afterAll(() => {
     mongoose.connection.close()
